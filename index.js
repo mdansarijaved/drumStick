c4841fe96a4c6293f2aa71b2cf8474979f76af0a
Javed Ansari <javedans2003@gmail.com>
2023-02-15 12:12:59 +0530

    added audio to drums

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
 document.querySelectorAll("button")[i].addEventListener("click",handleEvent)
 function handleEvent(){
-    alert("hello!")
+    var new_audio = new Audio('sounds/tom-1.mp3'); 
+    new_audio.play(); 
 }
 }
