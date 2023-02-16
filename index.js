4898cf237e9a99eb0a7b927707e30fca4401a925
Javed Ansari <javedans2003@gmail.com>
2023-02-16 20:24:55 +0530

    fixed console errors

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
-for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
-document.querySelectorAll("button")[i].addEventListener("click",handleEvent)
-function handleEvent(){
-    var new_audio = new Audio('sounds/tom-1.mp3'); 
-    new_audio.play(); 
+const keySound=(key)=>{
+  let new_audio;
+  switch (key) {
+    case "w":
+       new_audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+       new_audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+       new_audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+       new_audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+       new_audio = new Audio("sounds/crash.mp3");
+      break;
+    case "k":
+       new_audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    case "l":
+       new_audio = new Audio("sounds/snare.mp3");
+      break;
+    default:
+      break;
+  }
+  try {
+    new_audio.play();
+  } catch (error) {
+  }
 }
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", (e)=>{
+    keySound(e.target.innerText);
+  });
 }
+document.addEventListener("keypress",(e)=>{
+  const key=e.key;
+ keySound(key);
+})
+
+// function Madharchods(name, age, adress, experience) {
+//   this.name = name;
+//   this.age = age;
+//   this.adress = adress;
+//   this.experience = experience;
+//   this.clean = function(){
+//     alert("Cleaning the room sir!")
+//   }
+// }
+
+// var Madharchod1 = new Madharchods(
+//   "musadiq",
+//   21,
+//   "gand dene wali gali",
+//   "13years"
+// );
+
+// console.log(Madharchod1);
